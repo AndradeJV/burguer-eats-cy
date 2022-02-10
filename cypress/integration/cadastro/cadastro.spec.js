e176bc75e67b2972d5c8f301f1d66cdc7c5190b8
joao.andrade <joao.andrade@ecs.com.br>
2022-02-10 09:48:45 -0300

    Massa de dados externalizada para fixture

--- cypress/integration/cadastro/cadastro.spec.js
+++ cypress/integration/cadastro/cadastro.spec.js
@@ -1,32 +1,19 @@
 import signUp from '../../pages/SignUpPage';
 
 describe('Cadastro', () => {
-    it('Seja um entregador', () => {
-        
-        var entregador = {
-            nome: 'João Andrade',
-            cpf: '00000000012',
-            email: 'joaoteste@teste.com',
-            whatsApp: '11 91111-1111',
-            endereco: {
-                cep: '04534011',
-                rua: 'Rua Joaquim Floriano',
-                numero: 1000,
-                complemento: 'Ap 142',
-                bairro: 'Itaim Bibi',
-                cidadeUf: 'São Paulo/SP',
-            },
-            metodoEntrega: 'Moto',
-            cnh: '/cadastro/assets/cnh-digital.jpg'
-        }
+    beforeEach(function(){
+        cy.fixture('/cadastro/entregador').then((entregador) => {
+            this.entrega = entregador;
+        });
+    });
 
+    it('Seja um entregador', function(){
         const expectMessage = "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato."
 
 
         signUp.go();
-        signUp.fillForm(entregador);
+        signUp.fillForm(this.entrega.signUp);
         signUp.submit();
         signUp.modalContentShouldBe(expectMessage)
-        
     });
 });
