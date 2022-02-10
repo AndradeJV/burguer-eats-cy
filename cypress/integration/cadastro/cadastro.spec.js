9d1a5c44d38c00d43292cdc8c854d84e5d77f396
joao.andrade <joao.andrade@ecs.com.br>
2022-02-10 09:23:39 -0300

    Configurações base e page objects aplicado

--- cypress/integration/cadastro/cadastro.spec.js
+++ cypress/integration/cadastro/cadastro.spec.js
@@ -1,9 +1,8 @@
+import SignUpPage from '../../pages/SignUpPage';
+
 describe('Cadastro', () => {
     it('Seja um entregador', () => {
-        cy.viewport(1440, 900);
-        cy.visit('https://buger-eats.vercel.app');
-        cy.get('a[href="/deliver"]').click();
-        cy.get('form > h1').should('have.text', 'Cadastre-se para  fazer entregas');
+        const signUp = new SignUpPage();
 
         var entregador = {
             nome: 'João Andrade',
@@ -24,27 +23,11 @@ describe('Cadastro', () => {
 
         const expectMessage = "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato."
 
-        // Preenchimento de campos
-        cy.get('input[placeholder="Nome completo"]').type(entregador.nome);
-        cy.get('input[name="cpf"]').type(entregador.cpf);
-        cy.get('input[placeholder="E-mail"]').type(entregador.email);
-        cy.get('input[placeholder="Whatsapp"]').type(entregador.whatsApp);
 
-        cy.get('input[placeholder="CEP"]').type(entregador.endereco.cep);
-        cy.get('input[value="Buscar CEP"]').click();
-        cy.get('input[name="address-number"]').type(entregador.endereco.numero);
-        cy.get('input[name="address-details"]').type(entregador.endereco.complemento);
-
-        // Validação de campos
-        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua);
-        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro);
-        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidadeUf);
-
-        cy.contains('.delivery-method li', entregador.metodoEntrega).click();
-        cy.get('input[accept*="image"][type=file]').attachFile(entregador.cnh);
-
-        cy.get('form button[type=submit]').click();
-
-        cy.get('div[class="swal2-html-container"]').should('have.text', expectMessage);
+        signUp.go();
+        signUp.fillForm(entregador);
+        signUp.submit();
+        signUp.modalContentShouldBe(expectMessage)
+        
     });
 });
---
+++ cypress/pages/SignUpPage.js
@@ -0,0 +1,37 @@
+class SignUpPage {
+    go(){
+        cy.visit('/');
+        cy.get('a[href="/deliver"]').click();
+        cy.get('form > h1').should('have.text', 'Cadastre-se para  fazer entregas');
+    }
+
+    fillForm(entregador){
+        cy.get('input[placeholder="Nome completo"]').type(entregador.nome);
+        cy.get('input[name="cpf"]').type(entregador.cpf);
+        cy.get('input[placeholder="E-mail"]').type(entregador.email);
+        cy.get('input[placeholder="Whatsapp"]').type(entregador.whatsApp);
+
+        cy.get('input[placeholder="CEP"]').type(entregador.endereco.cep);
+        cy.get('input[value="Buscar CEP"]').click();
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero);
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento);
+
+        // Validação de campos
+        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua);
+        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro);
+        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidadeUf);
+
+        cy.contains('.delivery-method li', entregador.metodoEntrega).click();
+        cy.get('input[accept*="image"][type=file]').attachFile(entregador.cnh);
+    }
+
+    submit(){
+        cy.get('form button[type=submit]').click();
+    }
+
+    modalContentShouldBe(expectMessage){
+        cy.get('div[class="swal2-html-container"]').should('have.text', expectMessage);
+    }
+}
+
+export default SignUpPage;
